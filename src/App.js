058edb8bc7f4c60294a7d649ba93c163e87d15ab
Sarjid <sarjid@shuttlebd.com>
2023-02-22 16:41:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes ,Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './routes/home/home.component.jsx';
+import Navigation from './routes/navigation/navigation.compotent.jsx';
+
+
+const App = () => {
+
+    return (
+      <Routes>
+         <Route  path ='/' element={<Navigation/>}>
+              <Route index element={< Home />}/>
+          </Route>
+      </Routes>
+    );
 }
 
 export default App;
